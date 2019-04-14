1. Fix the lr_scheduler()\n2. Write log file as training\n3. Fix the calculation of accuracy

train.py
@@ -69,27 +69,9 @@ def main(args):
     # Push model to GPU
     model.to(device)
     # Define Loss and optimizer
-    print("[Using CrossEntropyLoss...]")
-    # criterion = nn.CrossEntropyLoss()
+    print("[Using BCEWithLogitsLoss...]")
     criterion = nn.BCEWithLogitsLoss()
-    # criterion = nn.MultiLabelSoftMarginLoss()
     print("[Using small learning rate with momentum...]")
-
-    # ##############
-    # # Parameters #
-    # ##############
-    # # Number of steps per iteration
-    # total_step = len(data_loader)
-    # # Data size (Each step train on a batch of 4 images)
-    # data_size = total_step*4
-    # # Learning rate
-    # learning_rate = 0.05
-    # # Number of times learning rate decay
-    # lr_changes = 5
-    # # Define optimizer
-    # optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, model.parameters())), lr=learning_rate, momentum=0.9)
-    # # Define learning rate scheduler
-    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=total_step//lr_changes, gamma=learning_rate//lr_changes)
 
     ##############
     # Parameters #
@@ -99,168 +81,109 @@ def main(args):
     # Data size (Each step train on a batch of 4 images)
     data_size = total_step*4
     # Learning rate
-    try:
+    if args.lr:
         learning_rate = args.lr # 0.05
-    except:
-        raise('Please provide learning rate')
+    else:
+        raise(ValueError('Please provide learning rate'))
     # Decay rate of learning rate
-    try:
+    if args.lr_decay:
         lr_decay = args.lr_decay # 5
-    except:
-        raise('Please provide rate of decay for learning rate')
+    else:
+        raise(ValueError('Please provide rate of decay for learning rate'))
     # Number of times learning rate decay
-    try:
-        lr_changes = args.lr_changes
-    except:
-        raise('Please provide number of decay times for learning rate')
+    if args.lr_changes:
+        lr_step = args.num_epochs//args.lr_changes
+    else:
+        raise(ValueError('Please provide number of decay times for learning rate'))
     # Define optimizer
     optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, model.parameters())), lr=learning_rate, momentum=0.9)
     # Define learning rate scheduler
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=total_step//lr_changes, gamma=lr_decay)
-
+    my_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=lr_step, gamma=lr_decay)
     ###################
     # Train the model #
     ###################
     # File name to save model
-    model_name = ''.join(['net_','epoch-',str(args.num_epochs),'_lr-',str(learning_rate),'_decay-',str(lr_decay),'.ckpt'])
+    model_name = ''.join(['net_','epoch-',str(args.num_epochs),'_lr-',str(learning_rate),'_decay-',str(lr_decay),'_step-',str(lr_step),'.ckpt'])
     # Move model to GPU
     model.to(device)
     # Start time
     start = time.time()
-    for epoch in range(args.num_epochs):
-        print('\nCurrent learning rate:{}\n'.format(get_lr(optimizer)))
-        t1 = time.time()
+    # Log file name
+    log_file = ''.join(['train_log/net_','epoch-',str(args.num_epochs),'_lr-',str(learning_rate),'_decay-',str(lr_decay),'_step-',str(lr_step),'.txt'])
+    # Check if file already exists
+    if os.path.isfile(log_file):
+        raise(Exception('File already exists'))
+    # If file not exists
+    else:
+        # Create log file
+        with open(log_file, 'w') as f:
+            for epoch in range(args.num_epochs):
+                # Change of learning rate
+                my_lr_scheduler.step()
+                print('-'*50)
+                print('Current learning rate:{}\n'.format(get_lr(optimizer)))
+                # Write log to file
+                f.write('-'*50)
+                f.write('\n')
+                f.write('Current learning rate:{}\n'.format(get_lr(optimizer)))
 
-        running_loss = 0.0
-        running_corrects = 0
-        for i, data in enumerate(data_loader):
+                t1 = time.time()
 
-            # mini-batch (Move input to GPU)
-            images = data[0].type(torch.FloatTensor).to(device)
-            # labels = data[1].type(torch.int64).to(device)
-            labels = data[1].type(torch.FloatTensor).to(device)
+                running_loss = 0.0
+                running_corrects = 0
+                for i, data in enumerate(data_loader):
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+                    # mini-batch (Move input to GPU)
+                    images = data[0].type(torch.FloatTensor).to(device)
+                    # labels = data[1].type(torch.int64).to(device)
+                    labels = data[1].type(torch.FloatTensor).to(device)
+                    
+                    # Forward, backward and optimize
+                    outputs,aux = model(images)
+                    # labels = torch.max(labels.long(), 1)[1]
+                    _, preds = torch.max(outputs.data, 1)
+                    # Compute the loss = primay net loss + 0.3 * auxilary net loss
+                    loss = criterion(outputs, labels) + 0.3*criterion(aux,labels)
+                    # Backprop the loss to the network (Compute the gradient of loss w.r.t parameters with require_grad = True)
+                    loss.backward()
+                    # Update the parameters within network
+                    optimizer.step()
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-            # Forward, backward and optimize
-            outputs,aux = model(images)
+                    # Current batch performance
+                    running_loss += loss
+                    labels = torch.max(labels.long(), 1)[1]
+                    running_corrects += torch.sum(preds == labels.data)
+                    # Log info
+                    if i % args.log_step == 0:
+                        print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch, args.num_epochs, i, total_step, loss.item()))
+                        # Write log to file
+                        f.write('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}\n'.format(epoch, args.num_epochs, i, total_step, loss.item()))
 
-            # labels = torch.max(labels.long(), 1)[1]
-            _, preds = torch.max(outputs.data, 1)
-            # print('\npred:{}\tlabel:{}\n'.format(preds[0].cpu().numpy(),labels))
-            # print('\noutputs:{}\tlabels:{}\n'.format(outputs.cpu().type(), labels.type()))
-            loss = criterion(outputs, labels) + 0.3*criterion(aux,labels)
-            loss.backward()
-            optimizer.step()
+                    # Save the model checkpoints
+                    if (i + 1) % args.save_step == 0:
+                        torch.save(model.state_dict(), os.path.join(args.model_path, model_name))
 
-            # Current batch performance
-            running_loss += loss
-            labels = torch.max(labels.long(), 1)[1]
-            running_corrects += torch.sum(preds == labels.data)
-            # Log info
-            if i % args.log_step == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                      .format(epoch, args.num_epochs, i, total_step, loss.item()))
-                # print('preds:{}\tlables:{}'.format(preds,labels))
+                epoch_loss = running_loss / data_size
+                epoch_acc = running_corrects.item() / data_size # running_corrects is a torch.Tensor()
+                print('Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+                # Write log to file
+                f.write('Loss: {:.4f} Acc: {:.4f}\n'.format(epoch_loss, epoch_acc))
 
-            # Save the model checkpoints
-            if (i + 1) % args.save_step == 0:
-                torch.save(model.state_dict(), os.path.join(args.model_path, 'net.ckpt'))
-
-        epoch_loss = running_loss / data_size
-        epoch_acc = running_corrects / data_size
-        print('Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
-
-        t2 = time.time()
-        print('Current epoch training time: {} minutes'.format((t2 - t1)/60))
+                t2 = time.time()
+                print('Current epoch training time: {} minutes'.format((t2 - t1)/60))
+                # Write log to file
+                f.write('Current epoch training time: {} minutes\n'.format((t2 - t1)/60))
 
     # End time
     end = time.time()
     torch.save(model.state_dict(), os.path.join(args.model_path, 'net.ckpt'))
     print('Total training times spent: {} minutes'.format((end - start)/60))
-
-########################
-# Self-defined Network #
-########################
-
-# def main(args):
-#     # Create model directory for saving trained models
-#     if not os.path.exists(args.model_path):
-#         os.makedirs(args.model_path)
-
-#     test_dataset = a2d_dataset.A2DDataset(train_cfg, args.dataset_path)
-#     data_loader = DataLoader(test_dataset, batch_size=4, shuffle=True, num_workers=4) # you can make changes
-
-
-#     # Define model
-#     model = net()
-#     # Push model to GPU
-#     model.to(device)
-#     # Define Loss and optimizer
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-#     # Define learning rate scheduler
-#     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-#     # Train the models
-#     total_step = len(data_loader)
-#     for epoch in range(args.num_epochs):
-#         t1 = time.time()
-#         running_loss = 0.0
-#         running_corrects = 0
-#         data_size = 0
-#         for i, data in enumerate(data_loader):
-
-#             # mini-batch
-#             images = data[0].to(device)
-#             labels = data[1].type(torch.FloatTensor).to(device)
-
-#             # zero the parameter gradients 
-#             optimizer.zero_grad()
-
-#             # Forward, backward and optimize
-#             outputs = model(images)
-
-#             # print('outputs:{}'.format(outputs.type()))
-#             # print('labels:{}'.format(labels.type()))
-
-#             # print('outputs:{}'.format(outputs.long()))
-#             # print('labels:{}'.format(labels.long()))
-
-#             labels = torch.max(labels.long(), 1)[1]
-#             # print('\noutputs:{}\tlabels:{}\n'.format(outputs.cpu().type(), labels.type()))
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
-
-#             # Current batch prediction
-#             _, preds = torch.max(outputs.data, 1)
-#             # Current batch performance
-#             running_loss += loss.data.item()
-#             running_corrects += torch.sum(preds == labels.data)
-
-#             data_size += 4
-
-
-#             # Log info
-#             if i % args.log_step == 0:
-#                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-#                       .format(epoch, args.num_epochs, i, total_step, loss.item()))
-
-#             # Save the model checkpoints
-#             if (i + 1) % args.save_step == 0:
-#                 torch.save(model.state_dict(), os.path.join(
-#                     args.model_path, 'net.ckpt'))
-
-#         print('data_size:{}.....'.format(data_size))
-#         epoch_loss = running_loss / data_size
-#         epoch_acc = running_corrects / data_size
-
-#         print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-
-#         t2 = time.time()
-#         print('Training times spent: {} minutes'.format((t2 - t1)/60))
+    # Write log to file
+    f.write('Total training times spent: {} minutes'.format((end - start)/60))
+    # Close the log file
+    f.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()