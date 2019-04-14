6206430722854b4741980638915660eb2d8d35ed
Kefu Zhu <kefuzhu@Kefus-MacBook-Pro.local>
2019-04-13 23:00:42 -0400

    Experiment with different loss function

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ def main(args):
         for i, param in model.named_parameters():
             param.requires_grad = False
 
-    # Edit the auxilary net 
+    # Edit the auxilary net
     num_ftrs = model.AuxLogits.fc.in_features
     model.AuxLogits.fc = nn.Linear(num_ftrs, n_class)
     # Edit the primary net
@@ -65,47 +65,52 @@ def main(args):
     model.to(device)
     # Define Loss and optimizer
     print("[Using CrossEntropyLoss...]")
-    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCEWithLogitsLoss()
+    # criterion = nn.MultiLabelSoftMarginLoss()
     print("[Using small learning rate with momentum...]")
-    optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, model.parameters())), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, model.parameters())), lr=0.05, momentum=0.9)
     # Define learning rate scheduler
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.01)
 
     # Train the models
 
     # Move model to GPU
     model.to(device)
     total_step = len(data_loader)
+    data_size = total_step*4
+    # Start time
+    start = time.time()
     for epoch in range(args.num_epochs):
         t1 = time.time()
-        data_size = 0
+
+        running_loss = 0.0
+        running_corrects = 0
         for i, data in tqdm(enumerate(data_loader)):
 
-            running_loss = 0.0
-            running_corrects = 0
             # mini-batch (Move input to GPU)
             images = data[0].type(torch.FloatTensor).to(device)
-            labels = data[1].type(torch.int64).to(device)
+            # labels = data[1].type(torch.int64).to(device)
+            labels = data[1].type(torch.FloatTensor).to(device)
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # Forward, backward and optimize
             outputs,aux = model(images)
-            labels = torch.max(labels.long(), 1)[1]
+
+            # labels = torch.max(labels.long(), 1)[1]
             _, preds = torch.max(outputs.data, 1)
             # print('\npred:{}\tlabel:{}\n'.format(preds[0].cpu().numpy(),labels))
             # print('\noutputs:{}\tlabels:{}\n'.format(outputs.cpu().type(), labels.type()))
-            loss = criterion(outputs, labels)
+            loss = criterion(outputs, labels) + 0.3*criterion(aux,labels)
             loss.backward()
             optimizer.step()
 
             # Current batch performance
             running_loss += loss
+            labels = torch.max(labels.long(), 1)[1]
             running_corrects += torch.sum(preds == labels.data)
-
-            data_size += 4
-
             # Log info
             if i % args.log_step == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
@@ -115,13 +120,17 @@ def main(args):
             if (i + 1) % args.save_step == 0:
                 torch.save(model.state_dict(), os.path.join(args.model_path, 'net.ckpt'))
 
-        print('data_size:{}.....'.format(data_size))
         epoch_loss = running_loss / data_size
         epoch_acc = running_corrects / data_size
         print('Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
 
         t2 = time.time()
-        print('Training times spent: {} minutes'.format((t2 - t1)/60))
+        print('Current epoch training time: {} minutes'.format((t2 - t1)/60))
+
+    # End time
+    end = time.time()
+    torch.save(model.state_dict(), os.path.join(args.model_path, 'net.ckpt'))
+    print('Total training times spent: {} minutes'.format((end - start)/60))
 
 ########################
 # Self-defined Network #
